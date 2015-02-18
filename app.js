1846755e2e259b4525b8d8f736075a1f03851672
David McElligott <damc.dev@gmail.com>
2015-02-17 21:06:33 -0600

    added post route

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
-var util = require('util');
 var CircularJSON = require('circular-json');
 
 var app = express();
 
-app.get('/api/request', function (req, res) {
+app.get('/api/request', getRequestObj );
+app.post('/api/request', getRequestObj);
+
+function getRequestObj(req, res) {
   console.log(req);
   res.setHeader('Content-Type', 'application/json');
   res.send(CircularJSON.stringify(req));
-});
+};
 
 var server = app.listen(1337, function () {
 
